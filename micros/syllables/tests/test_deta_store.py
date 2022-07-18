a3cb1550b5542d121a51ee87ff0c9d2cd34f5526
takecare <rui@vazteixeira.org>
2022-07-18 14:42:14 +0100

    backend: add db read test

--- micros/syllables/tests/test_deta_store.py
+++ micros/syllables/tests/test_deta_store.py
@@ -4,39 +4,28 @@ from pytest_mock import mocker
 from store.store import DetaStore
 
 
-class MockDeta:
-    def __init__(self) -> None:
-        print("init!!")
-        pass
-
-    class Base:
-        def __init__(self, name: str) -> None:
-            pass
-
-        def put(self, key: str, obj: Any):
-            pass
-
-        def read(self, key: str):
-            pass
-
-
-# @patch("store.store.Deta")
-# def test_write(mocked):
-def test_write(mocker):
-    # TODO mock out Deta
-    mock_Deta = mocker.Mock()
+# https://docs.python.org/3/library/unittest.mock.html#patch-object
+@patch("store.store.Deta")
+def test_write(mock_Deta, mocker):
     mock_Base = mocker.Mock()
 
-    # mock_Deta.return_value.Base.side_effect = mock_Base
     mock_Deta.return_value.Base.return_value = mock_Base
-
-    mocker.patch("store.store.Deta", mock_Deta)
-    # mocker.patch.object(mock_Deta, "Base", mock_Base)
-    # mocker.patch("store.store.Deta.Base", mock_Base)
+    # mocker.patch("store.store.Deta", mock_Deta)
 
     deta_store = DetaStore(name="debug", project_key="key")
     deta_store.write("key", {"data": "data"})
 
     mock_Base.put.assert_called()
-    # mock_Deta.Base.put.assert_called()
-    # mocked.Base.put.assert_called()
+
+
+@patch("store.store.Deta")
+def test_read(mock_Deta, mocker):
+    mock_Base = mocker.Mock()
+    mock_Base.get.return_value = {"key": "value"}
+    mock_Deta.return_value.Base.return_value = mock_Base
+
+    deta_store = DetaStore(name="debug", project_key="key")
+    data = deta_store.read("key")
+
+    mock_Base.get.assert_called()
+    assert data == {"key": "value"}
