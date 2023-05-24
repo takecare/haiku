361f9023773255e1c3b2372adf0fb95e919c1abd
Rui Teixeira <rui@vazteixeira.org>
2023-05-24 11:06:47 +0700

    remove old flaks config files

--- server/api/config_local.py
+++
@@ -1,4 +0,0 @@
-ENV = "development"
-DEBUG = True
-APP = "main.py"
-DETA_KEY = "xyz"
--- server/api/config_prod.py
+++
@@ -1,4 +0,0 @@
-ENV = "production"
-DEBUG = False
-APP = "main.py"
-DETA_KEY = "a07ZR2GfAfDr_v9PJYfUxBVj6ampWaSqFiWM5TrnCNdtU"
